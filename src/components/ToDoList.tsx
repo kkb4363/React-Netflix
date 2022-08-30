66fae5041130033971aaf21e3778c6b141874b42
gibeom <101088024+kkb4363@users.noreply.github.com>
2022-08-30 19:34:01 +0900

    #5.12~5.14
    if 연산자
    { A > B && C } : A>B가 참이면 C를 출력

--- src/components/ToDoList.tsx
+++ src/components/ToDoList.tsx
@@ -1,50 +1,19 @@
-import React from "react";
-import {useForm} from 'react-hook-form';
-import { atom, useRecoilState } from "recoil";
+import { useRecoilValue } from "recoil";
+import { toDoState } from "./atoms";
+import CreateToDo from "./CreateToDo";
+import ToDo from "./ToDo";
 
-const toDoState = atom<IToDo[]>({
-  key:'toDo',
-  default:[],
-})
-
-interface IForm{
-    toDo:string;
-}
-
-interface IToDo{
-  text:string;
-  id:number;
-  category:"TO_DO"|"DOING"|"DONE";
-
-}
 
 function ToDoList(){
-  const [ToDos, setToDos] = useRecoilState(toDoState);
- const {
-    register,
-    handleSubmit,
-    setValue
- } = useForm<IForm>()
-
- const handleValid = ({toDo}:IForm) => {
-    setToDos(prev => [{text:toDo , id:Date.now(), category:"TO_DO"}, ...prev])
-    setValue('toDo',"");
- }
- 
+  const ToDos = useRecoilValue(toDoState);
     return (
     <div>
       <h1>To Dos</h1>
       <hr/>
-      <form onSubmit={handleSubmit(handleValid)} >
-        <input {...register('toDo',
-        {
-            required:'please write a To Do',
-        })} placeholder="Write a to do"/>
-        <button>Add</button>
-      </form>
+      <CreateToDo/>
 
       <ul>
-        {ToDos.map(toDo => <li key={toDo.id}>{toDo.text}</li>)}
+        {ToDos.map(toDo => <ToDo key = {toDo.id} {...toDo}/>)}
       </ul>
     </div>
   );
