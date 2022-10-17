a747e2546f900bb6ace17fd4f70a4a4cbdf6343a
gibeom <kkb4363@naver.com>
2022-10-17 17:24:59 +0900

    배포수정

--- src/App.tsx
+++ src/App.tsx
@@ -5,14 +5,14 @@ import Search from './Routes/Search';
 import Header from './Components/Header';
 function App(){
   return (
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <BrowserRouter>
       <Header/>
       <Routes>
         <Route path="/tv" element={<Tv/>}/>
         <Route path="/tv/:tvId" element={<Tv/>}/>
         <Route path="/search" element={<Search/>}/>
         <Route path="/search/:Id" element={<Search/>}/>
-        <Route path='/' element={<Home/>}/>
+        <Route path={`${process.env.PUBLIC_URL}/`} element={<Home/>}/>
         <Route path="movies/:movieId" element={<Home/>}/>
       </Routes>
     </BrowserRouter>
